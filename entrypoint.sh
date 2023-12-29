6b72e5564af94a5e974a5a2f3aaec04aaaf84121
Suraj Shirvankar <surajshirvankar@gmail.com>
2023-12-29 02:31:09 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -39,6 +39,7 @@ fi
 
 { cd "$PAGES_GEM_HOME" || { echo "::error::pages gem not found"; exit 1; }; }
 
+bundle install
 # Run the command, capturing the output
 build_output="$($GITHUB_PAGES_BIN build "$VERBOSE" "$FUTURE" --source "$SOURCE_DIRECTORY" --destination "$DESTINATION_DIRECTORY")"
 
